fe544747d1f836ecdc6c75e063eb502508c7cd3c
JalesiaGriffin <jgriffin@appdev.yearup.org>
2023-10-24 09:32:39 -0400

    initial commit

---
+++ src/main/java/com/pluralsight/Tables.java
@@ -0,0 +1,25 @@
+package com.pluralsight;
+
+public class Tables {
+    public static void ledgerHeader() {
+        System.out.println(String.format("%s", "------------------------------------------------------------------------------------------------------------------------"));
+        System.out.println(String.format("%-25s %-25s %-30s %-25s %-25s", "Date", "Time",
+                "Description", "Vendor", "Amount"));
+        System.out.println(String.format("%s", "------------------------------------------------------------------------------------------------------------------------"));
+    }
+
+    public static void fillLedgerTable(Transaction t) {
+        System.out.println(String.format("%-25s %-25s %-30s %-25s %-25s", t.getDate(), t.getTime(),
+        t.getDescription(), t.getVendor(), t.getAmount()));
+    }
+
+    public static void reportsHeader() {
+        System.out.println(String.format("%s", "------------------------------------------------------------------------------------------------------------------------"));
+        System.out.println(String.format("%-25s %-25s %-25s", "Date", "Vendor", "Amount"));
+        System.out.println(String.format("%s", "------------------------------------------------------------------------------------------------------------------------"));
+    }
+
+    public static void fillReportsTable(Transaction t) {
+        System.out.println(String.format("%-25s %-25s %-25s", t.getDate(), t.getVendor(), t.getAmount()));
+    }
+}
